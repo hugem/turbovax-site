d00c1dee46cd22cbb550953754b94bd1aa2bb17b
hugem <huge.ma@gmail.com>
2021-02-01 08:08:35 -0500

    ok

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,8 +3,9 @@ import MaterialAlert from "@material-ui/lab/Alert";
 export default function Alert() {
   return (
     <MaterialAlert severity="warning">
-      <b>Update (1/29):</b> Both the city and state have begun to take
-      reservations again on the three major government vaccination portals.
+      <b>Update (2/1):</b> Due to the snowstorm, some existing resevervations
+      have been rescheduled. This may impact your ability to book new
+      reservations though we will continue to check the major vaccine portals.
     </MaterialAlert>
   );
 }
