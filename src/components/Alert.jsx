525408ba737e4eda5b1cc8e3147e8ea2d49f230b
hugem <huge.ma@gmail.com>
2021-02-01 08:02:31 -0500

    ok

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,8 +3,8 @@ import MaterialAlert from "@material-ui/lab/Alert";
 export default function Alert() {
   return (
     <MaterialAlert severity="warning">
-      <b>Update (1/29):</b> Authorities have resumed reservations at the three
-      big government vaccination centers.
+      <b>Update (1/29):</b> Both the city and state have begun to take
+      reservations again on the three major government vaccination portals.
     </MaterialAlert>
   );
 }
